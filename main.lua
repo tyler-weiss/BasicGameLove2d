2a5a9d0f9c94a7b53a4c1c64b5313c7176557bed
Tyler Weiss <1576859+tyler-weiss@users.noreply.github.com>
2024-02-17 20:00:36 -0500

    camera and boundaries to stop camera moving, need to contain character as well

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 -- Optional debugger to step through the love2d game
 if os.getenv("LOCAL_LUA_DEBUGGER_VSCODE") == "1" then
-    require("lldebugger").start()
+   require("lldebugger").start()
 end
 --- end of debugger
 
@@ -9,10 +9,11 @@ local STI = require("libraries.sti")
 function love.load()
    love.graphics.setDefaultFilter("nearest", "nearest")
    Map = STI("maps/tiletestmap.lua") -- Map exported from Tiled.
+   Camera = require('libraries.camera')
+   cam = Camera()
    require('player') -- Includes player, player animation
-   
 end
- 
+
 function love.update(dt)
    Player.currentAnimation = Player.animations.idle
    if love.keyboard.isDown("right") then
@@ -23,7 +24,7 @@ function love.update(dt)
       Player.x = Player.x - (Player.speed * dt)
       Player.currentAnimation = Player.animations.left
    end
- 
+
    if love.keyboard.isDown("down") then
       Player.y = Player.y + (Player.speed * dt)
       Player.currentAnimation = Player.animations.down
@@ -33,15 +34,44 @@ function love.update(dt)
       Player.currentAnimation = Player.animations.up
    end
 
-    if love.keyboard.isDown("r") then
-        Player.y = 0
-        Player.x = 0
-    end
-    Player.currentAnimation:update(dt)
+   if love.keyboard.isDown("r") then
+      Player.y = 0
+      Player.x = 0
+   end
 
+   Player.currentAnimation:update(dt)
+   cam:lookAt(Player.x, Player.y)
+
+   -- Get our game window dimentions
+   local w = love.graphics.getWidth()
+   local h = love.graphics.getHeight()
+   -- Left border
+   if cam.x < w / 2 then
+      cam.x = w / 2
+   end
+   -- Top border
+   if cam.y < h / 2 then
+      cam.y = h / 2
+   end
+
+   local mapW = Map.width * Map.tilewidth
+   local mapH = Map.height * Map.tileheight
+
+   -- Right border
+   if cam.x > (mapW - w / 2) then
+      cam.x = (mapW - w / 2)
+   end
+   -- Bottom border
+   if cam.y > (mapH - h / 2) then
+      cam.y = (mapH - h / 2)
+   end
 end
- 
+
 function love.draw()
-   Map:draw()
-   Player.currentAnimation:draw(Player.spriteSheet, Player.x, Player.y, nil, 1)
+   cam:attach()
+   Map:drawLayer(Map.layers['GrassLayer'])
+   Map:drawLayer(Map.layers['RoadTreeLayer'])
+   Player.currentAnimation:draw(Player.spriteSheet, Player.x, Player.y, nil, 1, nil, 45, 49)
+   cam:detach()
+   love.graphics.print(tostring(Player.x) .. ' ' .. tostring(Player.y))
 end
